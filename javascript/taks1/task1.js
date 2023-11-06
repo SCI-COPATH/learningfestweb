43fd1b3ca622cc841b4be4ceaf8d0da0ed281fa9
jayasankar J P <scicopath01@gmail.com>
2023-11-06 22:12:44 +0530

    Finish

--- javascript/taks1/task1.js
+++ javascript/taks1/task1.js
@@ -1,1 +1,8 @@
-console.log(Math.floor(Math.random()*6)+1)
+
+
+function findRandom(){
+    console.log("click")
+    let ra=Math.floor(Math.random()*6)+1
+    let res=document.getElementById('result')
+    res.innerText=ra
+}
